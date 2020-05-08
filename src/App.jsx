2e2224e506e7f2031891701c72149cfc02b3bf7f
CrisTo <toaquizacris@hotmail.com>
2020-05-08 14:40:02 -0500

    add style for posts & comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import Users from "./components/Users";
-import Posts from "./components/Posts";
-import CommentsMicroFrontend from "./components/CommentsMicroFrontend";
+import React from 'react';
+import Users from './components/Users';
+import Posts from './components/Posts';
+import CommentsMicroFrontend from './components/CommentsMicroFrontend';
 
 const App = () => (
   <main>
     <h1>App</h1>
     <Users />
-    <Posts />
-    <CommentsMicroFrontend />
+    <div style={{ display: 'flex' }}>
+      <Posts />
+      <CommentsMicroFrontend />
+    </div>
   </main>
 );
 
